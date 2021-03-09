d7a858eb68f14ff61321f2bd16390ed63d816fc9
OwO-18 <80306000+OwO-18@users.noreply.github.com>
2021-03-09 14:34:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,4 +37,5 @@ def handle_message(event):
 
     # Send To Line
     reply = TextSendMessage(text=f"{get_message}")
+    reply = TextSendMessage(text="123")
     line_bot_api.reply_message(event.reply_token, reply)
